61f93a2edbf45557575cd34c99f8cc9ca0b07139
Jairo <jairo.perez.ramon.info@gmail.com>
2024-05-13 00:28:13 +0200

    pagina login creado

---
+++ login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <div class = "form-body">
+        <p class="text"> Login FCT </p>
+        <form class="login-form">
+            <input type="text" placeholder="Introduce NIA">
+            <input type="password" placeholder="Introduce contraseña">
+            <button>Iniciar sesión</button>
+        </form>
+    </div>    
+</body>
+</html>
