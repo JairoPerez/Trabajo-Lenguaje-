d2b92a5091493828bbfaffad05ef5720eb66b7f0
Jairo <jairo.perez.ramon.info@gmail.com>
2024-05-14 13:34:55 +0200

    Login funcional creado correctamente

--- login.php
+++ login.php
@@ -5,76 +5,56 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-
     <?php
+    
         $host = "localhost";
-        $dbname = "universidad";
+        $dbname = "gestfct";
         $user = 'root';
         $pass='';
 
-        $sql="select * from alumno where true";
-            $datos=[];
 
-            if(!empty($dni)){
-                $sql.=' and dni like :dni';
-                $datos[':dni']=$dni;
-            }
+        try {
+            $conexion = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
+            $conexion->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
 
-            if(!empty($nombre)){
-                $sql.=' and nombre like :nombre';
-                $datos[':nombre']=$nombre;
-            }
-
-            try {
-            $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
-            $pdo->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-
-            $stmt=$pdo->prepare($sql);
-            $stmt->execute($datos);
-        
             }
 
             catch(PDOException $e) {
-                echo $e->getMessage();
+        echo $e->getMessage();
             }
-
-
+    
             
-
-            if ($_SERVER["REQUEST_METHOD"] == "POST") {
-                $username = $_POST['username'];
-                $password = $_POST['password'];
-                 // Realizar una consulta para verificar si el usuario existe y la contraseña es correcta
-                $sql = "SELECT id, username FROM usuarios WHERE username='$username' AND password='$password'";
-                $result = $conn->query($sql);
-
-                if ($result->num_rows == 1) {
-                // Usuario autenticado correctamente
-                $_SESSION['username'] = $username;
-                // Redirigir a la página de inicio o a donde desees
-                header("Location: Trabajo.php");
-                exit();
-                } else {
-             // Usuario o contraseña incorrectos
-                echo "Usuario o contraseña incorrectos";
+        if (!empty($_POST["btnacceder"])){
+            if (empty($_POST["nia"]) and empty($_POST["password"])){
+                    echo '<div> Los campos estan vacios </div>';
+    
+        } else {
+                
+            $nia=$_POST["nia"];
+            $password=$_POST["password"];
+    
+            $sql=$conexion->query(" select * from alumno where nia='$nia' and password='$password' ");
+    
+            if ($datos=$sql->FETCH()){
+                header("location:Buscador/Buscador.php");
+            }else{
+                echo '<div> Usuario o contraseña incorrectos </div>';
             }
         }
+    }
 
-
-    ?>
+ ?>
 
 </head>
 <body>
     <div class = "form-body">
         <p class="text"> Iniciar sesión</p>
         <form action="login.php" method="post">
-            
-            <input type="text" placeholder="Introduce NIA">
-            <input type="password" placeholder="Introduce contraseña">
 
-            <button type="submit">Iniciar sesión</button>
+            <input type="text" name="nia" placeholder="Introduce NIA">
+            <input type="password" name="password"  placeholder="Introduce contraseña">
+
+            <input type="submit" name="btnacceder">
             <button type="submit">¿Has olvidado la contraseña?</button>
-        </form>
-    </div>    
 </body>
 </html>
