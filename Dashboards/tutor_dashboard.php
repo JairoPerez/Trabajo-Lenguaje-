bb53e4d0e759726d21f838713acdb0a310bdfb3f
Jairo <jairo.perez.ramon.info@gmail.com>
2024-05-17 14:15:22 +0200

    Login funcional al 90%

---
+++ Dashboards/tutor_dashboard.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=H, initial-scale=1.0">
+    <title>Document</title>
+    <?php 
+    include '..\auth.php';
+    ?>
+</head>
+<body>
+    TUTOR
+</body>
+</html>
